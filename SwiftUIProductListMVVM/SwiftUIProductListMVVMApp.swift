28a42ede50a2d838558830442b7da180f458db0d
Nirav Virpara <virparanirav@gmail.com>
2025-06-13 16:18:02 +0530

    Initial Commit

---
+++ SwiftUIProductListMVVM/SwiftUIProductListMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProductListMVVMApp.swift
+//  SwiftUIProductListMVVM
+//
+//  Created by Nirav Virpara on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProductListMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
